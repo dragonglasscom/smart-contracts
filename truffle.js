3c22388078fc37f3bcdc34ed0728fe50f9d73aa7
Gleb Skibitsky <gl.skib@gmail.com>
2018-01-23 22:01:59 +0200

    Ropsten network config

--- truffle.js
+++ truffle.js
@@ -1,13 +1,20 @@
 module.exports = {
-   networks: {
+    networks: {
       development: {
         host: '95.85.20.19',
         port: 8080,
-        network_id: "*",
+        network_id: "*", // match any network
         gas: 4712388,
         gasPrice: 25000000000
-}},
+      },
+      ropsten: {
+        host: "localhost",
+        port: 8545,
+        network_id: 3
+      }
+    },
     rpc: {
-        host: '95.85.20.19',
-        port: 8080
-}};
+      host: 'localhost',
+      post:8080
+   }
+};
