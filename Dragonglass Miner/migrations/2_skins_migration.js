354070ddbfc3985eb73882fa3d7bd050e5f11c18
skibitsky <gl.skib@gmail.com>
2018-12-15 16:37:15 +0200

    New skins links updated

--- Dragonglass Miner/migrations/2_skins_migration.js
+++ Dragonglass Miner/migrations/2_skins_migration.js
@@ -9,13 +9,13 @@ var Masquerade = artifacts.require("./skins/Masquerade.sol");
 var OldMan = artifacts.require("./skins/OldMan.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Witcher, "https://dragonglass.com/i/DMWB.png");
-  deployer.deploy(WhiteWitcher, "https://dragonglass.com/i/DMWA.png");
-  deployer.deploy(King, "https://dragonglass.com/i/DMKA.png");
-  deployer.deploy(GlowingMushroom, "https://dragonglass.com/i/DMGA.png");
-  deployer.deploy(Prince, "https://dragonglass.com/i/DMPA.png");
-  deployer.deploy(SilverPickaxe, "https://dragonglass.com/i/DMSA.png");
-  deployer.deploy(JackPumkinhead, "https://dragonglass.com/i/DMJA.png");
-  deployer.deploy(Masquerade, "https://dragonglass.com/i/DMJA.png");
-  deployer.deploy(OldMan, "https://dragonglass.com/i/DMJA.png");
+  // deployer.deploy(Witcher, "https://dragonglass.com/i/DMWB.png");
+  // deployer.deploy(WhiteWitcher, "https://dragonglass.com/i/DMWA.png");
+  // deployer.deploy(King, "https://dragonglass.com/i/DMKA.png");
+  // deployer.deploy(GlowingMushroom, "https://dragonglass.com/i/DMGA.png");
+  // deployer.deploy(Prince, "https://dragonglass.com/i/DMPA.png");
+  // deployer.deploy(SilverPickaxe, "https://dragonglass.com/i/DMSA.png");
+  // deployer.deploy(JackPumkinhead, "https://dragonglass.com/i/DMJA.png");
+  deployer.deploy(Masquerade, "https://dragonglass.com/i/DMMA.png");
+  deployer.deploy(OldMan, "https://dragonglass.com/i/DMOA.png");
 };
